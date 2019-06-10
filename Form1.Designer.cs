86e8397965bcb4d46a88694eb57db65bdcb66193
Tejaswi kapoor <31186504+Tejaswikapoor@users.noreply.github.com>
2019-06-10 14:12:22 +0530

    windows application with database conectivity

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TEJAS
+{
+    partial class form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.dateOverTime = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblDob = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(209, 58);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(322, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(209, 105);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(322, 20);
+            this.txtHours.TabIndex = 1;
+            // 
+            // dateOverTime
+            // 
+            this.dateOverTime.Location = new System.Drawing.Point(209, 160);
+            this.dateOverTime.Name = "dateOverTime";
+            this.dateOverTime.Size = new System.Drawing.Size(322, 20);
+            this.dateOverTime.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(113, 58);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 20);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "NAME";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(113, 103);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(47, 20);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "AGE";
+            this.lblAge.UseWaitCursor = true;
+            // 
+            // lblDob
+            // 
+            this.lblDob.AutoSize = true;
+            this.lblDob.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDob.Location = new System.Drawing.Point(113, 161);
+            this.lblDob.Name = "lblDob";
+            this.lblDob.Size = new System.Drawing.Size(47, 20);
+            this.lblDob.TabIndex = 6;
+            this.lblDob.Text = "DOB";
+            this.lblDob.UseWaitCursor = true;
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 354);
+            this.Controls.Add(this.lblDob);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateOverTime);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.txtName);
+            this.Name = "form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.DateTimePicker dateOverTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblDob;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEJAS
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=(LocalDB)\mydb;Initial Catalog=Test_app;Integrated security=true";
+
+            cnn = new SqlConnection(connetionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+
+            cmd.CommandText= ("INSERT  into ExtraWork VALUES (@Name,@Hours,@Date)");
+            int age;
+            if (int.TryParse(txtHours.Text, out age))
+            {
+                if (age > 25)
+                {
+                    MessageBox.Show("The number of hours cannot be greater than 8 ! Please correct the entered value !");
+                    return;
+                }
+                else
+                {
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please insert an integer into Hours textbox !");
+                return;
+            }
+
+            
+            cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Hours", age);
+            cmd.Parameters.AddWithValue("@Date", dateOverTime.Value);
+
+            try
+            {
+                cnn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("The age was added ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection ! ");
+            }
+
+          
+        }
+    }
+}
